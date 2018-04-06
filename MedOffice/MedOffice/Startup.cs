eb4c74e6c5efb888fc8e97e66efd5632dec02a20
Weronika Naziemiec <wnaziemiec@gmail.com>
2018-04-06 22:16:17 +0200

    rola Admina i sam Admin

--- MedOffice/MedOffice/Startup.cs
+++ MedOffice/MedOffice/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MedOffice.Models;
 
 [assembly: OwinStartupAttribute(typeof(MedOffice.Startup))]
 namespace MedOffice
@@ -9,6 +12,46 @@ namespace MedOffice
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+
+        // metoda do tworzenia roli i Admina  
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // tworzymy role admina i samego Admina    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // tworzymy role admina   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                // tworzymy usera                 
+                var user = new ApplicationUser();
+                user.UserName = "naszAdmin";
+                user.Email = "naszAdmin@gmail.com";
+
+                string userPWD = "A@Z200711";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //przypisujemy uzytkownikowi role admina  
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // pozostale role
         }
     }
 }
