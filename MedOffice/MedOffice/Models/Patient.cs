a316b6d14b277512095f9d9d0141402dbab43d47
gorzz <38137609+gorzz@users.noreply.github.com>
2018-04-16 20:33:34 +0200

    Pacjent - czesc 1
    Dodałam model pacjenta wraz z kontrolerem, widokiem i bazą danych.
    
    Teraz muszę to dopracować. Aby panel pacjenta dostępny był tylko dla określonych ról. Mam też nadzieję, połączyć to jakoś z historią chorób i wizyt.
    
    Przy pisaniu korzystałam z tego tutorialu: https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/creating-a-connection-string

---
+++ MedOffice/MedOffice/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MedOffice.Models
+{
+    [Table("Patients")]
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+        public string Pesel { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class PatientDBContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
