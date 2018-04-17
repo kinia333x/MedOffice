93af166c2100117165276aac5aac41f1f9026e88
Kamilason <kamilason@interia.pl>
2018-04-17 14:50:39 +0200

    Search
    Dodanie SearchController'a i widoku wyszukiwania - ogarniecie wyszukiwania pracownikow po Imieniu, Nazwisku, Peselu i Specjalizacji

---
+++ MedOffice/MedOffice/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using MedOffice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedOffice.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public ActionResult WorkerSearch(string searching)
+        {
+            // kryteria wyszukiwania
+            return View(context.Users.Where(x=>x.UserName.Contains(searching) 
+                                            || x.Name.Contains(searching)
+                                            || x.Surname.Contains(searching)
+                                            || x.Specialization.Contains(searching)
+                                            || searching == null).ToList());
+        }
+    }
+}
