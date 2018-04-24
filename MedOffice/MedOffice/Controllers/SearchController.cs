c12b08f979f2fd2c6e69a56664ccce63439a716a
Kamilason <kamilason@interia.pl>
2018-04-24 20:01:25 +0200

    Szukajki
    Dodano wyszukiwarke pacjentów, ale nie ma dalej jebanego sortowania ani dla pacjentow ani dla pracownikow i pracownicy tez nie mają wyswietlanego zawodu bo chuj wie jak połączyć 2 tabele na podstawie trzeciej zeby jeszcze sie do odwolywalo do jednego modelu w widoku

--- MedOffice/MedOffice/Controllers/SearchController.cs
+++ MedOffice/MedOffice/Controllers/SearchController.cs
@@ -11,16 +11,56 @@ namespace MedOffice.Controllers
     {
         // GET: Search
 
-        ApplicationDbContext context = new ApplicationDbContext();
+        
+        
 
-        public ActionResult WorkerSearch(string searching)
+        public ActionResult WorkerSearch(string searching, int sortOpt = 1)
         {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            //var list = from s in context.Users
+            //           join sa in context.Roles on s.Id equals sa.Id
+            //           select new
+            //           {
+            //               UserName = s.UserName,
+            //               Name = s.Name,
+            //               Role = sa.Name,
+            //               Specialization = s.Specialization,
+            //               Surname = s.Surname
+
+            //           };
+
+
             // kryteria wyszukiwania
-            return View(context.Users.Where(x=>x.UserName.Contains(searching) 
+            var userList = context.Users.Where(x => x.UserName.Contains(searching)
+                                              || x.Name.Contains(searching)
+                                              || x.Surname.Contains(searching)
+                                              || x.Specialization.Contains(searching)
+                                              || searching == null);
+            switch (sortOpt) // to gówno nie działa
+            {
+                case 1: userList.ToList().OrderBy(s => s.Name); break;
+                case 2: userList.ToList().OrderBy(s => s.Surname); break;
+                case 3: userList.ToList().OrderBy(s => s.UserName); break;
+                case 4: userList.ToList().OrderBy(s => s.Specialization); break;
+            }
+            
+            return View(userList);
+        }
+
+
+        
+
+        public ActionResult PatientSearch(string searching)
+        {
+            PatientDBContext patientContext = new PatientDBContext();
+            // kryteria wyszukiwania
+            List<Patient> patientList = patientContext.Patients.Where(x => x.Pesel.Contains(searching)
                                             || x.Name.Contains(searching)
                                             || x.Surname.Contains(searching)
-                                            || x.Specialization.Contains(searching)
-                                            || searching == null).ToList());
+                                            || searching == null).ToList();
+            patientList.OrderBy(s => s.Surname);
+            return View(patientList);
         }
     }
 }
