53fb8a761d82ec603d7f80908ec7a3030770e437
gorzz <38137609+gorzz@users.noreply.github.com>
2018-05-01 17:20:43 +0200

    Poprawki pacjenta II
    Dodanie:
    1. Unikalnego numeru pesel
    2. Poprawienie regexa adresu
    3. Sprawdzanie numeru peselu ze względu na datę urodzenia

--- MedOffice/MedOffice/Models/Patient.cs
+++ MedOffice/MedOffice/Models/Patient.cs
@@ -26,6 +26,8 @@ namespace MedOffice.Models
 
         [Required(ErrorMessage = "Pesel jest wymagany.")]
         [Display(Name = "Pesel:")]
+        [PeselUnique]
+        [PeselAndDateOfBirth("BirthDate")]
         [RegularExpression("([0-9]{11})", ErrorMessage = "Pole pesel musi zawierać 11 cyfr.")]
         public string Pesel { get; set; }
 
@@ -37,7 +39,7 @@ namespace MedOffice.Models
 
         [Required(ErrorMessage = "Adres jest wymagany.")]
         [Display(Name = "Adres:")]
-        [RegularExpression(@"^[0-9]{2}[\-][0-9]{3}[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}(?:\,[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}\s[0-9]+(?:\/[0-9]*)?)?$", ErrorMessage = "Pole Adres musi mieć strukturę: 00-000 Miasto[, Ulica 00[/00]]. Dopuszczalne są polskie znaki.")]
+        [RegularExpression(@"^[0-9]{2}[\-][0-9]{3}[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}(?:\,[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,})?\s[0-9]+(?:\/[0-9]*)?$", ErrorMessage = "Pole Adres musi mieć strukturę: 00-000 Miasto[, Ulica] 00[/00]. Dopuszczalne są polskie znaki.")]
         public string Address { get; set; }
     }
 
@@ -51,11 +53,77 @@ namespace MedOffice.Models
             }
             else
             {
-                return new ValidationResult("Data urodzenia musi być w przeszłości");
+                return new ValidationResult("Data urodzenia musi być w przeszłości.");
             }
         }
     }
+    public class PeselUniqueAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PatientDBContext db = new PatientDBContext();
+            var patients = db.Patients.FirstOrDefault(p => p.Pesel == (string)value);
 
+            if (patients == null)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Pacjent o padanym peselu już istnieje.");
+            }
+        }
+    }
+    public class PeselAndDateOfBirthAttribute : ValidationAttribute
+    {
+        private string _birthDateName { get; set; }
+        public PeselAndDateOfBirthAttribute(string birthDateName)
+        {
+            _birthDateName = birthDateName;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string _value = value.ToString();
+            var _birthDateProperty = validationContext.ObjectType.GetProperty(_birthDateName);
+            if (_birthDateProperty == null)
+            {
+                return new ValidationResult("Error!");
+            }
+
+            DateTime _birthDate = (DateTime)_birthDateProperty.GetValue(validationContext.ObjectInstance, null);
+
+            var dt = _birthDate.ToString("yyyyMMdd");
+
+            if (dt.ToString()[2] != _value[0])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Rok jest niepoprawny.");
+            }
+            if (dt.ToString()[3] != _value[1])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Rok jest niepoprawny.");
+            }
+
+            if (dt.ToString()[4] != _value[2])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Miesiąc jest niepoprawny.");
+            }
+            if (dt.ToString()[5] != _value[3])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Miesiąc jest niepoprawny.");
+            }
+
+            if (dt.ToString()[6] != _value[4])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Dzień jest niepoprawny.");
+            }
+            if (dt.ToString()[7] != _value[5])
+            {
+                return new ValidationResult("Data urodzenia oraz pesel nie zgadzają się. Dzień jest niepoprawny.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
     public class PatientDBContext : DbContext
     {
         public DbSet<Patient> Patients { get; set; }
