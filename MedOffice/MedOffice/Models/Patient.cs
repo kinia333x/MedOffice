0d61d77c9ae788d06188bfc0c887f97f5a48774e
gorzz <38137609+gorzz@users.noreply.github.com>
2018-05-01 10:19:33 +0200

    Poprawki do pacjenta
    Naprawienie:
    1. Koniec z rejestracją pacjenta, który się jeszcze nie urodził (jendak na to trzeba będzie znaleźć lepsze rozwiązanie, bo na razie validacja jest wtedy gdy wszystkie inne pola są poprawnie wypełnione)
    2. Dodanie przycisku "Pacjenci" dla kierownika
    3. Poprawienie regexa dla Adresu
    4. Poprawienie opisu błedu dla Imienia i Nazwiska

--- MedOffice/MedOffice/Models/Patient.cs
+++ MedOffice/MedOffice/Models/Patient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -14,12 +15,12 @@ namespace MedOffice.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Pole Imię jest wymagane.")]
-        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$", ErrorMessage = "Pole Imię może zawierać wyłącznie litery. Dopuszczalne są polskie znaki.")]
+        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$", ErrorMessage = "Pole imię musi mieć strukturę: Imię. Może zawierać wyłącznie litery. Dopuszczalne są polskie znaki.")]
         [Display(Name = "Imię:")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Pole Nazwisko jest wymagane.")]
-        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?$", ErrorMessage = "Pole Nazwisko może zawierać wyłącznie litery. Dopuszczalne są nazwiska dwuczłonowe i zawierające polskie znaki.")]
+        [RegularExpression(@"^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)?$", ErrorMessage = "Pole Nazwisko musi mieć strukturę: Nazwisko. Może zawierać wyłącznie litery. Dopuszczalne są nazwiska dwuczłonowe i zawierające polskie znaki.")]
         [Display(Name = "Nazwisko:")]
         public string Surname { get; set; }
 
@@ -31,12 +32,28 @@ namespace MedOffice.Models
         [Required(ErrorMessage = "Data urodzenia jest wymagana.")]
         [Display(Name = "Data urodzenia:")]
         [DataType(DataType.Date, ErrorMessage = "DATA")]
+        [PastDate]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "Adres jest wymagany.")]
         [Display(Name = "Adres:")]
-        [RegularExpression(@"^[0-9]{2}[\-][0-9]{3}[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\,[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}\s[0-9]+(?:\/[0-9]*)?$", ErrorMessage = "Pole Adres musi mieć strukturę: 00-000 Miasto, Ulica 00[/00]. Dopuszczalne są polskie znaki.")]
+        [RegularExpression(@"^[0-9]{2}[\-][0-9]{3}[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}(?:\,[\s][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+([\s\-][A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+){0,}\s[0-9]+(?:\/[0-9]*)?)?$", ErrorMessage = "Pole Adres musi mieć strukturę: 00-000 Miasto[, Ulica 00[/00]]. Dopuszczalne są polskie znaki.")]
         public string Address { get; set; }
+    }
+
+    public class PastDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && (DateTime)value < DateTime.Now)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Data urodzenia musi być w przeszłości");
+            }
+        }
     }
 
     public class PatientDBContext : DbContext
