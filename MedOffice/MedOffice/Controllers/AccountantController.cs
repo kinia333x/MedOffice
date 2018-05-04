9f94b28db80904329d75aa36508960a11c252266
gorzz <38137609+gorzz@users.noreply.github.com>
2018-05-04 15:40:48 +0200

    Dodanie generacji pdf-a i panelu księgowej.
    Dodołam panel dla księgowej, oraz generację raportu (korzystając z pakietu MvcRazorToPdf).

---
+++ MedOffice/MedOffice/Controllers/AccountantController.cs
@@ -0,0 +1,28 @@
+﻿using MedOffice.Models;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcRazorToPdf;
+
+namespace MedOffice.Controllers
+{
+    [Authorize(Roles = "Administrator, Kierownik, Księgowa")]
+    public class AccountantController : Controller
+    {
+        private AppointmentDBContext db = new AppointmentDBContext();
+
+        // GET: Accountant
+        public ActionResult Index()
+        {
+            return View(db.Appointments.ToList());
+        }
+        
+        public ActionResult Report()
+        {
+            return new PdfActionResult("Report", db.Appointments.ToList());
+        }
+    }
+}
