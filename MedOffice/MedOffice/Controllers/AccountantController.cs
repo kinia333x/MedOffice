ab2e11d4a0a2d84c98c5e5ae14d63213a2c8c761
gorzz <38137609+gorzz@users.noreply.github.com>
2018-05-11 19:02:20 +0200

    Pdf - edycja, wyszukiwanie
    Dodałam do pdf możliwość edycji (na podstawie Kingi edycji) oraz możliwość wyszukiwania (na podstawie Kamila wyszukajek). Trzeba zrobić: wybieranie tylko niektórych pól do raportu i chyba jednak zmienić coś z geracją pdf-a, bo nie generują się polskie znaki.

--- MedOffice/MedOffice/Controllers/AccountantController.cs
+++ MedOffice/MedOffice/Controllers/AccountantController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcRazorToPdf;
+using System.Net;
+using System.Data.Entity;
 
 namespace MedOffice.Controllers
 {
@@ -14,12 +16,47 @@ namespace MedOffice.Controllers
     {
         private AppointmentDBContext db = new AppointmentDBContext();
 
-        // GET: Accountant
-        public ActionResult Index()
+        public ActionResult Index(string searching, string sortOrder)
         {
-            return View(db.Appointments.ToList());
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.PriceSortParm = sortOrder == "price" ? "price_desc" : "price";
+            ViewBag.AddPriceSortParm = sortOrder == "addPrice" ? "addPrice_desc" : "addPrice";
+            ViewBag.TotalPriceSortParm = sortOrder == "totalPrice" ? "totalPrice_desc" : "totalPrice";
+            ViewBag.DataSortParm = sortOrder == "data" ? "data_desc" : "data";
+
+            var services = db.Appointments.Where(x => x.service_name.Contains(searching)
+                                          || x.service_price.ToString().Contains(searching)
+                                          || x.supplies_price.ToString().Contains(searching)
+                                          || (x.supplies_price + x.service_price).ToString().Contains(searching)
+                                          || searching == null)
+                                          .Select(a => new AppointmentViewModels.ServiceViewModel()
+                                          {
+                                              Id = a.ID,
+                                              ServiceType = a.service_type,
+                                              ServiceName = a.service_name,
+                                              ServiceDate = a.appoint_date,
+                                              ServicePrice = a.service_price,
+                                              SuppliesPrice = a.supplies_price,
+                                              TotalPrice = a.service_price + a.supplies_price
+                                          });
+
+            switch (sortOrder)
+            {
+                case "name_desc": services = services.OrderByDescending(s => s.ServiceName); break;
+                case "price": services = services.OrderBy(s => s.ServiceName); break;
+                case "price_desc": services = services.OrderByDescending(s => s.ServicePrice); break;
+                case "addPrice": services = services.OrderBy(s => s.ServicePrice); break;
+                case "addPrice_desc": services = services.OrderByDescending(s => s.SuppliesPrice); break;
+                case "totalPrice": services = services.OrderBy(s => s.TotalPrice); break;
+                case "totalPrice_desc": services = services.OrderByDescending(s => s.TotalPrice); break;
+                case "data": services = services.OrderBy(s => s.ServiceDate); break;
+                case "data_desc": services = services.OrderByDescending(s => s.ServiceDate); break;
+                default: services = services.OrderBy(s => s.ServiceName); break;
+            }
+
+            return View(services.ToList());
         }
-        
+
         public ActionResult Report()
         {
             return new PdfActionResult("Report", db.Appointments.ToList());
---
+++ MedOffice/MedOffice/Models/AppointmentViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MedOffice.Models
+{
+    public class AppointmentViewModels
+    {
+        public class ServiceViewModel
+        {
+            public int Id { get; set; }
+
+            [Display(Name = "Typ usługi:")]
+            public string ServiceType { get; set; }            //TYP USŁUGI
+            [Display(Name = "Nazwa usługi:")]
+            public string ServiceName { get; set; }            //nazwa usługi
+            [Display(Name = "Data usługi:")]
+            public DateTime ServiceDate { get; set; }          //DATA WIZYTY
+            [Display(Name = "Cena usługi:")]
+            public float ServicePrice { get; set; }            //cena usługi
+            [Display(Name = "Koszty dodatkowe:")]
+            public float SuppliesPrice { get; set; }           //dodatkowe koszty
+            [Display(Name = "Suma kosztów:")]
+            public float TotalPrice { get; set; }              //suma ceny usługi i kosztów dodatkowych
+        }
+    }
+}
